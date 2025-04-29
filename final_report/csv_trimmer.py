679fc0d9a6b695d4d554064355bdc0c550400893
Will Geister <wgeister@wisc.edu>
2025-04-29 17:34:19 -0500

    started the math portion on the final report

--- final_report/csv_trimmer.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the CSV
-df = pd.read_csv("master_stocks.csv")
-
-df_no_timestamp = df.drop(columns=["timestamp"])
-
-df_no_timestamp.to_csv("master_stocks.csv", index=False)
